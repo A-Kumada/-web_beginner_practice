5e2c04ce5883ff9d5fd2234cb9c1d43e6eef20db
Ayano Kumada <ayano@AyanonoMacBook-Pro.local>
2023-04-04 12:57:39 +0900

    first commit

--- webrick.rb
+++ webrick.rb
@@ -1,14 +1,19 @@
- # webrick.rb
 require 'webrick'
+require "erb"
 
-server = WEBrick::HTTPServer.new({ 
+server = WEBrick::HTTPServer.new({
   :DocumentRoot => './',
   :BindAddress => '127.0.0.1',
   :Port => 8000
 })
 
-trap(:INT){
-    server.shutdown
-}
+WEBrick::HTTPServlet::FileHandler.add_handler("erb", WEBrick::HTTPServlet::ERBHandler)
+server.config[:MimeTypes]["erb"] = "text/html"
+
+server.mount_proc("/hello") do |req, res|
+  template = ERB.new( File.read('hello.erb') )
+    @time = Time.new
+  res.body << template.result( binding )
+end
 
 server.start
